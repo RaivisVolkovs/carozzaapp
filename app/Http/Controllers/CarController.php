08b2ca583a8a875f3072b880f071eda72dddc27a
RaivisVolkovs <raivis.volkovs14@gmail.com>
2024-01-01 17:04:07 +0000

    Implemented edit feature

--- app/Http/Controllers/CarController.php
+++ app/Http/Controllers/CarController.php
@@ -24,8 +24,9 @@ class CarController extends Controller
 
     public function create() {
 
+        $cars= new Car();
         $manufacturers= Manufacturer::orderBy('name')->pluck('name', 'id')->prepend('All Manufacturers', '');
-        return view('cars.create', compact('manufacturers'));
+        return view('cars.create', compact('manufacturers', 'cars'));
     }
 
     public function show($id) {
@@ -45,5 +46,28 @@ class CarController extends Controller
 
         Car::create($request->all());
         return redirect()->route('cars.index')->with('message', 'Car has been added successfully!');
+    }
+
+    public function edit($id) {
+
+        $cars= Car::find($id);
+        $manufacturers= Manufacturer::orderBy('name')->pluck('name', 'id')->prepend('All Manufacturers', '');
+        return view('cars.edit', compact('manufacturers', 'cars'));
+    }
+
+    public function update($id, Request $request) {
+
+        $request->validate([
+            'model'=>'required',
+            'year'=>'required',
+            'salesperson_email'=>'required|email',
+            'manufacturer_id'=>'required|exists:manufacturers,id'
+        ]);
+
+
+        $car = Car::find($id); 
+        $car->update($request->all());
+
+        return redirect()->route('cars.index')->with('message', 'Contact has been updated successfully!');
     }
 }
